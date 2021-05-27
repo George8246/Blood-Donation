9213d0b289337cc3eb52670c3cfa098836b5d33d
yousufehab <yousufehab02@gmail.com>
2021-05-27 18:07:01 +0200

    creating classes(user,donor,hospital)

---
+++ controllers/Donor.js
@@ -0,0 +1,9 @@
+class Donor extends User {
+  constructor(DonorID, BloodGroup, DphoneNumber, Health, age) {
+    this.DonorID = DonorID;
+    this.BloodGroup = BloodGroup;
+    this.DphoneNumber = DphoneNumber;
+    this.Health = Health;
+    this.age = age;
+  }
+}
--- controllers/User.js
+++ controllers/User.js
@@ -1,20 +1,11 @@
 class User {
-    constructor(userName, Password, Address) {
-        this.userName = userName;
-        this.Password = Password;
-        this.Address = Address;
-    }
+  constructor(userName, Password, Address) {
+    this.userName = userName;
+    this.Password = Password;
+    this.Address = Address;
+  }
 
-    constructor(userName, Password) {
-        this.userName = userName;
-        this.Password = Password;
-    }
+  Registration(userName, Password, Address) {}
 
-    Resistration(userName, Password, Address) {
-        
-    }
-
-    LogIN(userName, Password) {
-        
-    }
+  LogIN(userName, Password) {}
 }
